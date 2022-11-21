3c10066aed324439fb5d0043f73553f21d5ae785
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-21 18:39:15 -0300

    feat: added migration script to startup

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
@@ -70,7 +64,7 @@ namespace PlatformService
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
-            PrepDb.PrepPopulation(app);
+            PrepDb.PrepPopulation(app, _environment.IsProduction());
         }
     }
 }
