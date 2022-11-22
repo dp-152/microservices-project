b844786f7e4f32037d332a6448db93959294c280
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-22 15:02:06 -0300

    feat: added controller for Commands

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
