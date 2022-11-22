5a8a809499fa7614482fe26b30d12cabaf3ed220
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-22 14:31:36 -0300

    feat: added AutoMapper service to startup

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +29,7 @@ namespace CommandsService
         {
 
             services.AddControllers();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
