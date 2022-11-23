04d769d4a0b62406b09ce5087a8135e41762d44e
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-23 19:26:05 -0300

    feat: implemented interface

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
