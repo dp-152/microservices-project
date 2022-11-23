706a721df123867c85e7704696ba29293bdfa36b
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-23 19:27:12 -0300

    feat: determining event type

--- CommandsService/EventProcessing/EventProcessor.cs
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Dtos;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CommandsService.EventProcessing
@@ -16,7 +19,24 @@ namespace CommandsService.EventProcessing
 
         public void ProcessEvent(string message)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            Console.WriteLine("--->> Determining event...");
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch (eventType.Event)
+            {
+                case "Platform_Publish":
+                    Console.WriteLine("--->> Platform Publish Event detected");
+                    return EventType.PlatformPublish;
+                
+                default:
+                    Console.WriteLine("--->> Could not determine event type");
+                    return EventType.Undetermined;
+            }
         }
     }
 
