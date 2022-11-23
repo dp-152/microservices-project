08bdc21209c4c1239f71a9bc4fe4e90c40af48a1
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-23 14:59:37 -0300

    feat: declared implementation for message bus client

---
+++ PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using PlatformService.Dtos;
+using RabbitMQ.Client;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var factory = new ConnectionFactory
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+                
+                _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+                _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+                
+                Console.WriteLine("--->> Connected to Message Bus");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--->> Could not connect to Message Bus: {ex.Message}");
+            }
+        }
+
+        public void PublishNewPlatform(PlatformPublishDto platformData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("--->> RabbitMQ Connection Shutdown");
+        }
+    }
+}
