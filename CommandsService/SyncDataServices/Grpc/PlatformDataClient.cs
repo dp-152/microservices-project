4d37dbc6fcc9d716c2b9a8110af44321ec829e44
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-24 00:52:04 -0300

    feat: calling GRPC service

--- CommandsService/SyncDataServices/Grpc/PlatformDataClient.cs
+++ CommandsService/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AutoMapper;
 using CommandsService.Models;
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
+using PlatformService;
 
 namespace CommandsService.SyncDataServices.Grpc
 {
@@ -18,7 +21,22 @@ namespace CommandsService.SyncDataServices.Grpc
 
         public IEnumerable<Platform> ReturnAllPlatforms()
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine($"--->> Calling GRPC Service: {_configuration["GrpcPlatform"]}");
+
+            var channel = GrpcChannel.ForAddress(_configuration["GrpcPlatform"]);
+            var client = new GrpcPlatform.GrpcPlatformClient(channel);
+            var request = new GetAllRequest();
+
+            try
+            {
+                var reply = client.GetAllPlatforms(request);
+                return _mapper.Map<IEnumerable<Platform>>(reply.Platform);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--->> Could not call GRPC Server: {ex.Message}");
+                throw;
+            }
         }
     }
 }
