b087dfdfed6a9ab32b5a26c2970ba79f50f2d280
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-24 00:34:07 -0300

    feat: implemented GRPC Client

---
+++ CommandsService/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace CommandsService.SyncDataServices.Grpc
+{
+    public class PlatformDataClient : IPlatformDataClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public PlatformDataClient(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<Platform> ReturnAllPlatforms()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
