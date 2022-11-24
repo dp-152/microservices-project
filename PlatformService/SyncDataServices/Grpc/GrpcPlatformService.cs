d86cf45d2a52e880745a18160b94f4386d0c6502
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-24 00:07:48 -0300

    feat: implemented GRPC service

---
+++ PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Grpc.Core;
+using PlatformService.Data;
+
+namespace PlatformService.SyncDataServices.Grpc
+{
+    public class GrpcPlatformService : GrpcPlatform.GrpcPlatformBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public GrpcPlatformService(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override Task<GetAllResponse> GetAllPlatforms(GetAllRequest request, ServerCallContext context)
+        {
+            var response = new GetAllResponse();
+
+            var platforms = _repository.GetAllPlatforms();
+
+            foreach (var platform in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GrpcPlatformModel>(platform));
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
